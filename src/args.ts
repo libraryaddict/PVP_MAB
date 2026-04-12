03fd2a772b8b27c40a061231ac89d75839c54f4a
libraryaddict <libraryaddict@users.noreply.github.com>
2026-04-12 15:35:20 +1200

    Allow win/loss messages, allow challenging harder opponents

--- src/args.ts
+++ src/args.ts
@@ -1,6 +1,7 @@
 import { Args } from "grimoire-kolmafia";
 import { canInteract } from "kolmafia";
 import { Strategy } from "./lib";
+import { get } from "libram";
 
 type PvpTarget = "fame" | "loot" | "flowers";
 
@@ -36,6 +37,20 @@ export const args = Args.create("pvp_mab", "A multi-armed bandit script for pvp"
     (x) => x as Strategy,
     "multi-armed bandit strategy"
   ),
+  winMessage: Args.string({
+    help: "Message to show on fight win, defaults to 'defaultFlowerWinMessage'",
+    // The default is an empty string
+    default: get("defaultFlowerWinMessage"),
+  }),
+  lossMessage: Args.string({
+    help: "Message to show on fight loss, defaults to 'defaultFlowerLossMessage'",
+    // The default is an empty string
+    default: get("defaultFlowerLossMessage"),
+  }),
+  ranked: Args.flag({
+    help: "Fight random tougher opponents for +1 swagger and increased fame gain",
+    default: false,
+  }),
   no_optimize: Args.flag({
     help: "Skip the uberpvpoptimizer step",
     default: false,
